ae0dd168834bf2b14878381bd9ddb1c0fbadd01d
kalevishal <kalevishal1610@gmail.com>
2021-10-22 22:01:24 +0530

    Added: Multiple rides and return aggregate for all.

--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -1,4 +1,6 @@
 import com.cabinvoicegenerator.CabInvoiceGenerator;
+import com.cabinvoicegenerator.PrimiumRides;
+import com.cabinvoicegenerator.Rides;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,14 +9,25 @@ public class CabInvoiceTest {
     @Test
     void givenDistanceAndTime_ShouldReturnTotalFare() {
         CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-        double TotalFare = cabInvoiceGenerator.CalculateFare(5.0, 4);
+        double TotalFare = cabInvoiceGenerator.calculateFare(5.0, 4);
         Assertions.assertEquals(54, TotalFare);
     }
 
     @Test
     void givenDistanceAndTime_ShouldReturnMinFare() {
         CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-        double TotalFare = cabInvoiceGenerator.CalculateFare(0.1, 2);
+        double TotalFare = cabInvoiceGenerator.calculateFare(0.1, 2);
         Assertions.assertEquals(5, TotalFare);
+    }
+
+    @Test
+    void givenMultipleRides_ShouldCalculateAggregateTotal() {
+
+        Rides[] rides = {new Rides(1.0, 5, PrimiumRides.Normal),
+                new Rides(0.2, 2, PrimiumRides.Normal)};
+
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double totalFare = cabInvoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(20, totalFare, 0.0);
     }
 }
